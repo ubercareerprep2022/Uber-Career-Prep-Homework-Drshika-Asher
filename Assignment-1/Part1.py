50254b2c9b9728a2317058fcab7815bdf3f5a34c
Drshika Asher <drshika@Drshikas-MacBook-Pro.local>
2022-03-10 17:48:32 -0600

    Solution for 2.1

--- Assignment-1/Part1.py
+++ Assignment-1/Part1.py
@@ -1,8 +1,10 @@
+# 2.1
 def isStringPermutation(s1: str, s2: str) -> bool:
-    return False
+    return sorted(s1) == sorted(s2)
 
 
-
+#Runtime O(log n) as sorted takes O(log n) time to sort an array of chars
+#Space time O(n) since this is in place
 
 #Test Cases
 print(isStringPermutation('asdf', 'fsda') == True)
