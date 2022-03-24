ab5ba7b0d7bd406052643a06e5dfb189d2cc8bc2
Drshika A <67125579+drshika@users.noreply.github.com>
2022-03-24 10:16:26 -0500

    Update Part2.py

--- Assignment-1/Part2.py
+++ Assignment-1/Part2.py
@@ -3,7 +3,7 @@ def isStringPermutation(s1: str, s2: str) -> bool:
     return sorted(s1) == sorted(s2)
 
 
-#Runtime O(log n) as sorted takes O(log n) time to sort an array of chars
+#Runtime O(log n) as sorted takes O(nlog n) time to sort an array of chars
 #Space time O(n) since this is in place
 
 #Test Cases
