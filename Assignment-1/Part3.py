ed415b5934174b7de27651c5d70b626b59dae75d
Drshika Asher <67125579+drshika@users.noreply.github.com>
2022-04-14 18:35:34 -0500

    add nullchecks

--- Assignment-1/Part3.py
+++ Assignment-1/Part3.py
@@ -2,7 +2,7 @@ import sys
 # Challenge: Implement the Stack class from scratch(do not use your language’s standard stack or queue library/package methods).
 # In this challenge, your Stack will only accept Integer values. Implement the following methods:
 
-# okay so basically i started with the approach of making the stack as an array and only add and delete from the front. 
+# basically i started with the approach of making the stack as an array and only add and delete from the front. 
 # some edge cases I was considering is making sure that my class works for multiple stack instances, 
 # removing more than you add, isempty on an empty stack, top, pop and min on an empty stack.
 
@@ -36,7 +36,7 @@ class stack:
 
     # # push() → Pushes an integer on top of the stack
     def push(self, val):
-        if self.head == None:
+        if self.isEmpty():
             self.head = Node(val)
             self.global_min = self.head.data
         else:
@@ -48,18 +48,26 @@ class stack:
         self.stack_size += 1
     # pop() → Removes what is on the top of the stack, and returns that value to the caller
     def pop(self):
+        if self.isEmpty():
+            return "Nothing to pop"
         to_return = self.head.data
         self.head = self.head.next
         self.stack_size -= 1
         return to_return
     # top() → Looks at the top value, and returns it. Does not manipulate the stack
     def top(self):
+        if self.isEmpty():
+            return "Nothing on top"
         return self.head.data
     # size() → Returns an integer value with the count of elements in the stack
     def size(self):
+        if self.isEmpty():
+            return "Nothing in stack"
         return self.stack_size
     # min() → Returns an integer value of the smallest element in the stack
     def min(self):
+        if self.isEmpty():
+            return "Nothing in stack"        
         return self.global_min
 
 myStack = stack()
